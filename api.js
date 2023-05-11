a353be739dec0661685c1ee2d09d0d019f3d034e
Kiril Yanev <kl_yanev@abv.bg>
2023-05-11 16:27:36 +0300

    Add API requests

--- api.js
+++ api.js
@@ -2,6 +2,10 @@
 
 const host = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 
-function request() {
-    
+async function request(url) {
+    const response = await fetch(host + url);
+    const result = await response.json();
+    return result;
 }
+
+export const getData = request;
--- app.js
+++ app.js
@@ -1,9 +1,49 @@
+import { getData } from "./api.js";
 
-const button = document.querySelector('button');
-button.addEventListener('click',getWeatherData);
+const selectElement = document.querySelector('select');
+const nextButton = document.querySelector('button');
+nextButton.addEventListener('click', getCountryes);
 
+// TO ADD EVENTLISTENER ONCHANGE TO selectElement;
 
-function getWeatherData() {
+async function getCountryes() {
+    let countryesData = {};
+    
     const input = document.querySelector('input');
     const cityName = input.value;
+
+    // If no input return
+    if (cityName === '') {
+        return;
+    }
+    const url = `${cityName}&limit=10&appid=4ac8da0dc9053f374e5e587c102978a9`;
+    const data = await getData(url);
+
+    selectElement.innerHTML = '';
+
+    data.forEach(cityData => {
+        if (!countryesData.hasOwnProperty(cityData.country)) {
+
+            countryesData[cityData.country] = {
+                name: cityData.name,
+                lat: cityData.lat,
+                lon: cityData.lon
+            }
+            const option = document.createElement('option');
+            option.textContent = cityData.country;
+            selectElement.appendChild(option);
+        }
+    });
+
+    renderForecastData(data[0]);
+}
+
+async function renderForecastData(data) {
+    
+    const lat = data.lat;
+    const lon = data.lon;
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4ac8da0dc9053f374e5e587c102978a9`);
+    const forecastData = await response.json();
+    console.log(forecastData);
 }
