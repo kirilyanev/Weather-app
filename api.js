5b6ff60f749ae1ebf1348d2bc2f51ab94ef66ba7
Kiril Yanev <kl_yanev@abv.bg>
2023-05-15 13:36:50 +0300

    Update api.js

--- api.js
+++ api.js
@@ -8,4 +8,28 @@ async function request(url) {
     return result;
 }
 
+
+async function currentWeather(data) {
+    const lat = data.lat;
+    const lon = data.lon;
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4ac8da0dc9053f374e5e587c102978a9`);
+    const currentWeather = await response.json();
+
+    return currentWeather;
+}
+
+async function hourlyForecast(data) {
+    const lat = data.lat;
+    const lon = data.lon;
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=4ac8da0dc9053f374e5e587c102978a9`);
+    const hourlyForecast = await response.json();
+
+    return hourlyForecast;
+}
+
+
 export const getData = request;
+export const getCurrentWeather = currentWeather;
+export const getHourlyForecast = hourlyForecast;
