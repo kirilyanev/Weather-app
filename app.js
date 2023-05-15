b23cb4c462e0aaab6a779d2533625d4c399d7959
Kiril Yanev <kl_yanev@abv.bg>
2023-05-15 13:38:41 +0300

    Update app.js
    Move API calls from app.js to api.js

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 import { getData } from "./api.js";
+import { getCurrentWeather } from "./api.js";
+import { getHourlyForecast } from "./api.js";
 import { renderCurrentWeather } from "./render.js";
+import { renderForecast } from "./render.js";
 
-// const weatherIcon = document.querySelector('.weather-info img');
-// const tempIcon = document.querySelector('.temperature');
-// const descriptionElement = document.querySelector('.description');
-// const cityName = document.querySelector('#city-name');
-// const stateName = document.querySelector('#state-name');
-// const humidityElement = document.querySelector('#humidity');
-// const pressureElement = document.querySelector('#pressure');
+
 
 const selectElement = document.querySelector('select');
 const nextButton = document.querySelector('button');
 nextButton.addEventListener('click', getCountryes);
 
-// TO ADD EVENTLISTENER ONCHANGE TO selectElement;
+// TO ADD EVENTLISTENER ONCHANGE TO selectElement;git
 
 async function getCountryes() {
     let countryesData = {};
@@ -26,11 +23,11 @@ async function getCountryes() {
         return;
     }
     const url = `${cityName}&limit=10&appid=4ac8da0dc9053f374e5e587c102978a9`;
-    const data = await getData(url);
+    const citiesData = await getData(url);
 
     selectElement.innerHTML = '';
 
-    data.forEach(cityData => {
+    citiesData.forEach(cityData => {
         if (!countryesData.hasOwnProperty(cityData.country)) {
 
             countryesData[cityData.country] = {
@@ -44,38 +41,11 @@ async function getCountryes() {
         }
     });
 
-    const weatherData = await getForecastData(data[0]);
-    // renderWeatherData(weatherData)
-    renderCurrentWeather(weatherData)
+    const currentWeatherData = await getCurrentWeather(citiesData[0]);
+    const forecastData = await getHourlyForecast(citiesData[0]);
+
+    renderCurrentWeather(currentWeatherData);
+    renderForecast(forecastData);
 
 }
-
-async function getForecastData(data) {
-    
-    const lat = data.lat;
-    const lon = data.lon;
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4ac8da0dc9053f374e5e587c102978a9`);
-    const forecastData = await response.json();
-    
-    return forecastData;
-}
-
-// async function renderWeatherData(weatherData) {
-//     const iconCode = weatherData.weather[0].icon;
-//     // Celsius = Kelvin – 273.15
-//     const temp = (Number(weatherData.main.temp) - 273.15).toFixed(1);
-//     const weatherDescription = weatherData.weather[0].description;
-
-//     weatherIcon.src = 'http://openweathermap.org/img/w/' + iconCode + '.png';
-//     tempIcon.textContent = temp + '°C';
-//     descriptionElement.textContent = weatherDescription;
-    
-//     cityName.textContent = 'City: ' + weatherData.name; 
-//     stateName.textContent = 'State: ' + weatherData.sys.country;
-//     humidityElement.textContent = 'Humidity: ' + weatherData.main.humidity + '%';
-//     pressureElement.textContent = 'Pressure: ' + weatherData.main.pressure + ' mb';
-
-//     console.log(weatherData);
-// }
 
