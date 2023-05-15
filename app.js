bd78f327771a73ff251ccf83460b76563fea0e61
Kiril Yanev <kl_yanev@abv.bg>
2023-05-15 12:32:54 +0300

    Create render.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import { getData } from "./api.js";
+import { renderCurrentWeather } from "./render.js";
 
-const weatherIcon = document.querySelector('.weather-info img');
-const tempIcon = document.querySelector('.temperature');
-const descriptionElement = document.querySelector('.description');
-const cityName = document.querySelector('#city-name');
-const stateName = document.querySelector('#state-name');
-const humidityElement = document.querySelector('#humidity');
-const pressureElement = document.querySelector('#pressure');
+// const weatherIcon = document.querySelector('.weather-info img');
+// const tempIcon = document.querySelector('.temperature');
+// const descriptionElement = document.querySelector('.description');
+// const cityName = document.querySelector('#city-name');
+// const stateName = document.querySelector('#state-name');
+// const humidityElement = document.querySelector('#humidity');
+// const pressureElement = document.querySelector('#pressure');
 
 const selectElement = document.querySelector('select');
 const nextButton = document.querySelector('button');
@@ -44,7 +45,9 @@ async function getCountryes() {
     });
 
     const weatherData = await getForecastData(data[0]);
-    renderWeatherData(weatherData)
+    // renderWeatherData(weatherData)
+    renderCurrentWeather(weatherData)
+
 }
 
 async function getForecastData(data) {
@@ -58,20 +61,21 @@ async function getForecastData(data) {
     return forecastData;
 }
 
-async function renderWeatherData(weatherData) {
-    const iconCode = weatherData.weather[0].icon;
-    // Celsius = Kelvin – 273.15
-    const temp = (Number(weatherData.main.temp) - 273.15).toFixed(1);
-    const weatherDescription = weatherData.weather[0].description;
+// async function renderWeatherData(weatherData) {
+//     const iconCode = weatherData.weather[0].icon;
+//     // Celsius = Kelvin – 273.15
+//     const temp = (Number(weatherData.main.temp) - 273.15).toFixed(1);
+//     const weatherDescription = weatherData.weather[0].description;
 
-    weatherIcon.src = 'http://openweathermap.org/img/w/' + iconCode + '.png';
-    tempIcon.textContent = temp + '°C';
-    descriptionElement.textContent = weatherDescription;
+//     weatherIcon.src = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+//     tempIcon.textContent = temp + '°C';
+//     descriptionElement.textContent = weatherDescription;
     
-    cityName.textContent = 'City: ' + weatherData.name; 
-    stateName.textContent = 'State: ' + weatherData.sys.country;
-    humidityElement.textContent = 'Humidity: ' + weatherData.main.humidity + '%';
-    pressureElement.textContent = 'Pressure: ' + weatherData.main.pressure + ' mb';
+//     cityName.textContent = 'City: ' + weatherData.name; 
+//     stateName.textContent = 'State: ' + weatherData.sys.country;
+//     humidityElement.textContent = 'Humidity: ' + weatherData.main.humidity + '%';
+//     pressureElement.textContent = 'Pressure: ' + weatherData.main.pressure + ' mb';
 
-    console.log(weatherData);
-}
+//     console.log(weatherData);
+// }
+
---
+++ render.js
@@ -0,0 +1,29 @@
+
+const weatherIcon = document.querySelector('.weather-info img');
+const tempIcon = document.querySelector('.temperature');
+const descriptionElement = document.querySelector('.description');
+const cityName = document.querySelector('#city-name');
+const stateName = document.querySelector('#state-name');
+const humidityElement = document.querySelector('#humidity');
+const pressureElement = document.querySelector('#pressure');
+
+
+async function renderWeatherData(weatherData) {
+    const iconCode = weatherData.weather[0].icon;
+    // Celsius = Kelvin – 273.15
+    const temp = (Number(weatherData.main.temp) - 273.15).toFixed(1);
+    const weatherDescription = weatherData.weather[0].description;
+
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+    tempIcon.textContent = temp + '°C';
+    descriptionElement.textContent = weatherDescription;
+    
+    cityName.textContent = 'City: ' + weatherData.name; 
+    stateName.textContent = 'State: ' + weatherData.sys.country;
+    humidityElement.textContent = 'Humidity: ' + weatherData.main.humidity + '%';
+    pressureElement.textContent = 'Pressure: ' + weatherData.main.pressure + ' mb';
+
+    console.log(weatherData);
+}
+
+export const renderCurrentWeather = renderWeatherData;
