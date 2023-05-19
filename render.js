bdf2591ac7e59638b2d7fcd218debb5972425a5b
Kiril Yanev <kl_yanev@abv.bg>
2023-05-19 23:02:08 +0300

    Update render.js
    Update all table forecast data

--- render.js
+++ render.js
@@ -81,87 +81,60 @@ async function renderForecastData(forecastData) {
       </tr>
       <tr>
         <!-- SAMPLE TEMPLATE FOR HOURLY FORECAST -->
-        <td>
-          <div>
-            ${(data.list[0].dt_txt.split(' ')[1]).slice(0, 2) + ' h'}
-          </div>
-          <div>
-            ${(data.list[0].main.temp - 273.15).toFixed(1) + 'C°'}
-          </div>
-          <img src="${'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png'}" alt="Rain icon"
-            class="icon">
-        </td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 0)}
+        ${trTemplate(data, 8)}
+        ${trTemplate(data, 16)}
+        ${trTemplate(data, 24)}
+        ${trTemplate(data, 32)}
       </tr>
       <tr>
-        <td>
-          <div>
-            ${(data.list[1].dt_txt.split(' ')[1]).slice(0, 2) + ' h'}
-          </div>
-          <div>
-            ${(data.list[1].main.temp - 273.15).toFixed(1) + 'C°'}
-          </div>
-          <img src="${'http://openweathermap.org/img/w/' + data.list[1].weather[0].icon + '.png'}" alt="Rain icon"
-            class="icon">
-        </td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 1)}
+        ${trTemplate(data, 9)}
+        ${trTemplate(data, 17)}
+        ${trTemplate(data, 25)}
+        ${trTemplate(data, 33)}
       </tr>
       <tr>
-        <td>
-          <div>
-            ${(data.list[2].dt_txt.split(' ')[1]).slice(0, 2) + ' h'}
-          </div>
-          <div>
-            ${(data.list[2].main.temp - 273.15).toFixed(1) + 'C°'}
-          </div>
-          <img src="${'http://openweathermap.org/img/w/' + data.list[2].weather[0].icon + '.png'}" alt="Rain icon"
-            class="icon">
-        </td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 2)}
+        ${trTemplate(data, 10)}
+        ${trTemplate(data, 18)}
+        ${trTemplate(data, 26)}
+        ${trTemplate(data, 34)}
       </tr>
       <tr>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 3)}
+        ${trTemplate(data, 11)}
+        ${trTemplate(data, 19)}
+        ${trTemplate(data, 27)}
+        ${trTemplate(data, 35)}
       </tr>
       <tr>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 4)}
+        ${trTemplate(data, 12)}
+        ${trTemplate(data, 20)}
+        ${trTemplate(data, 28)}
+        ${trTemplate(data, 36)}
       </tr>
       <tr>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 5)}
+        ${trTemplate(data, 13)}
+        ${trTemplate(data, 21)}
+        ${trTemplate(data, 29)}
+        ${trTemplate(data, 37)}
       </tr>
       <tr>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 6)}
+        ${trTemplate(data, 14)}
+        ${trTemplate(data, 22)}
+        ${trTemplate(data, 30)}
+        ${trTemplate(data, 38)}
       </tr>
       <tr>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
-        <td>hh - temp - img</td>
+        ${trTemplate(data, 7)}
+        ${trTemplate(data, 15)}
+        ${trTemplate(data, 23)}
+        ${trTemplate(data, 31)}
+        ${trTemplate(data, 39)}
       </tr>
     </table>
     `;
@@ -169,6 +142,44 @@ async function renderForecastData(forecastData) {
   render(template(forecastData), tableElement);
   // console.log(forecastData);
 }
+
+
+// SAMPLE TEMPLATE
+{/* <td>
+<div>
+  ${(data.list[0].dt_txt.split(' ')[1]).slice(0, 2) + ' h'}
+</div>
+<div>
+  ${(data.list[0].main.temp - 273.15).toFixed(1) + 'C°'}
+</div>
+<img src="${'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png'}" alt="Rain icon"
+  class="icon">
+</td> */}
+
+const hourTemplate = (data, n) => {
+  return `${(data.list[n].dt_txt.split(' ')[1]).slice(0, 2) + ' h'}`;
+}
+
+const tempTemplate = (data, n) => {
+  return `${(data.list[n].main.temp - 273.15).toFixed(1) + 'C°'}`;
+}
+
+const imgTemplate = (data, n) => {
+  return `${'http://openweathermap.org/img/w/' + data.list[n].weather[0].icon + '.png'}`
+}
+
+const trTemplate = (data, n) => html`
+<td>
+  <div id="hours-forecast">
+    ${hourTemplate(data, n)}
+  </div>
+  <div id="temp-forecast">
+    ${tempTemplate(data, n)}
+  </div>
+  <img src="${imgTemplate(data, n)}" alt="Rain icon" class="icon">
+</td>`
+
+
 
 export const renderCurrentWeather = renderWeatherData;
 export const renderForecast = renderForecastData;
